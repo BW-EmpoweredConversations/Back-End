728c51fa190c9b595ee3e5bc61a23e33c313ce3f
ericbanker12 <ericbanker12@gmail.com>
2019-11-18 15:48:22 -0600

    add POST for login

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -29,7 +29,7 @@ router.post('/register', async (req, res) => {
     const email = req.body.email
     try {
         const user = await authModel.findUser({email})
-        if (user) return res.status(409).json({message: `The email address is already in use on another account. Please enter a different email address.`})
+        if (user) return res.status(409).json({message: `That email address is already in use. Please enter a different email address.`})
     }
     catch (err) {
         console.error(err)
@@ -51,7 +51,7 @@ router.post('/register', async (req, res) => {
     authModel.addUser({email, password, name, phone_number})
         .then(user => {
             // make token
-            jwt.sign({id: user.id}, process.env.SECRET || 'secret key', (err, authorization) => {
+            jwt.sign({id: user.id}, process.env.SECRET || 'secret key', {expiresIn: '7d'}, (err, authorization) => {
                 if (err) throw err
                 // send user data and authorization token
                 res.status(201).json({user, authorization})
@@ -61,4 +61,56 @@ router.post('/register', async (req, res) => {
             console.error(err)
             res.sendStatus(500)
         })
+})
+
+router.post('/login', async (req, res) => {
+
+    // check body
+    if (!req.body) return res.status(400).json({message: 'Missing content application/json'})
+
+    // check fields and types
+    for (let prop of ['email', 'password']) {
+        if (!req.body[prop]) return res.status(400).json({message: 'Missing required property: ' + prop})
+
+        if (typeof req.body[prop] != 'string') return res.status(400).json({message: `Property ${prop} must be a string`})
+    }
+
+    // check email pattern
+    if (!/\S+@\S+\.\S+/.test(req.body.email)) return res.status(400).json({message: `Property email did not match expected pattern. Pattern example: yourname@website.com`})
+
+    // find user
+    let user
+    try {
+        const email = req.body.email
+        user = await authModel.findUser({email})
+        if (!user) return res.status(401).json({message: `Incorrect login information`})
+    }
+    catch (err) {
+        console.error(err)
+        res.sendStatus(500)
+    }
+
+    // check password
+    try {
+        const match = await bcrypt.compare(req.body.password, user.password)
+        if (!match) return res.status(401).json({message: `Incorrect login information`})
+    }
+    catch (err) {
+        console.error(err)
+        res.sendStatus(500)
+    }
+
+    // make token
+    jwt.sign({id: user.id}, process.env.SECRET || 'secret key', {expiresIn: '7d'}, (err, authorization) => {
+        if (err) {
+            console.error(err)
+            res.sendStatus(500)
+        }
+        else {
+            // sanitize password
+            delete user.password
+            // send user data and authorization token
+            res.json({user, authorization})
+        }
+    })
 })
