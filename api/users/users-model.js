9e7324ae337f8ab6c09aacc9f9efa27032c465c3
ericbanker12 <ericbanker12@gmail.com>
2019-11-19 11:41:57 -0600

    add PUT to update user phone number by user id

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -3,6 +3,7 @@ const db = require('../../database/database')
 module.exports = {
     findUser,
     findUserNoAuth,
+    updateUser,
 }
 
 function findUser(filter) {
@@ -13,4 +14,12 @@ function findUser(filter) {
 function findUserNoAuth(filter) {
     if (filter) return db('users').select('id', 'email', 'name', 'phone_number').where(filter).first()
     else return db('users').select('id', 'email', 'name', 'phone_number')
+}
+
+function updateUser(id, changes) {
+    try {
+        return db('users').where({id}).update(changes)
+            .then(resp => findUserNoAuth({id}))
+    }
+    catch (err) {throw err}
 }
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -18,3 +18,38 @@ router.get('/:id', valUserAuth, (req, res) => {
             res.sendStatus(500)
         })
 })
+
+router.put('/:id', valUserAuth, (req, res) => {
+
+    // check body
+    if (!req.body) return res.status(400).json({message: 'Missing content application/json'})
+
+    // check fields and types
+    for (let prop of ['phone_number']) {
+        if (!req.body[prop]) return res.status(400).json({message: 'Missing required property: ' + prop})
+
+        if (typeof req.body[prop] != 'string') return res.status(400).json({message: `Property ${prop} must be a string`})
+    }
+    
+    // check phone_number length
+    const phone_number = req.body.phone_number.replace(/\D/g,'')
+    if (phone_number.length < 10 || phone_number.length > 11) return res.status(400).json({message: `Property phone_number has the wrong number of digits. Pattern example: 555-555-5555`})
+
+    const id = Number(req.params.id)
+    
+    userModel.findUserNoAuth({id})
+        .then(user => {
+            if (user) {
+                userModel.updateUser(id, {phone_number})
+                    .then(user => {
+                        res.json(user)
+                    })
+            }
+            else res.sendStatus(404)
+        })
+        .catch(err => {
+            console.error(err)
+            res.sendStatus(500)
+        })
+})
+
